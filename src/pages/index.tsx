bca3bc66a066790c399974a0a71aeb28b3d07992
Kim Miso <kimms9725@naver.com>
2023-04-05 20:00:39 +0900

    change Head Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,6 +53,7 @@ FeedPage.getLayout = function getlayout(page) {
         description: CONFIG.blog.description,
         type: "website",
         url: CONFIG.link,
+        image: CONFIG.profile.image,
       }}
     >
       {page}
